jaydeepkumar1984/cassandra

Update IAutoRepairTokenRangeSplitter API to be more flexible
Updates IAutoRepairTokenRangeSplitter API to take in a set of tables
instead of a single table and return "RepairAssignments".

This allows the API more control over how the repairs are issued by
the AutoRepair framework, by allowing grouping of repairs by specific
tables or a single repair at the keyspace level.

Also updates IAutoRepairTokenRangeSplitter to be a ParameterizedClass,
so custom splitters can have their own configuration.

Update AutoRepair to account for these changes.

Update DefaultAutoRepairTokenSplitter to account for the change, and
to also evaluate on getRepairByKeyspace option.  This also improves
the filtering of tables that should not be repaired where previously
getRepairByKeyspace would not factor this in.

Move logic to split token ranges evenly into AutoRepairUtils so it
can be reutilized by other splitters.

Adds prototype UnrepairedBytesBasedTokenRange that demonstrates
consuming splitter-based properties to limit the repair assignments
returned to be bound.

src/java/org/apache/cassandra/repair/autorepair/DefaultAutoRepairTokenSplitter.java
@@ -20,7 +20,10 @@ package org.apache.cassandra.repair.autorepair;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.cassandra.service.AutoRepairService;
 
@@ -33,15 +36,18 @@ import org.apache.cassandra.dht.Token;
 import org.apache.cassandra.service.StorageService;
 import org.apache.cassandra.utils.Pair;
 
+import static org.apache.cassandra.repair.autorepair.AutoRepairUtils.splitEvenly;
+
 public class DefaultAutoRepairTokenSplitter implements IAutoRepairTokenRangeSplitter
 {
     private static final Logger logger = LoggerFactory.getLogger(DefaultAutoRepairTokenSplitter.class);
 
 
     @Override
-    public List<Pair<Token, Token>> getRange(AutoRepairConfig.RepairType repairType, boolean primaryRangeOnly, String keyspaceName, String tableName)
+    public List<RepairAssignment> getRepairAssignments(AutoRepairConfig.RepairType repairType, boolean primaryRangeOnly, String keyspaceName, Set<String> tableNames)
     {
-        List<Pair<Token, Token>> range = new ArrayList<>();
+        AutoRepairConfig config = AutoRepairService.instance.getAutoRepairConfig();
+        List<RepairAssignment> repairAssignments = new ArrayList<>();
 
         Collection<Range<Token>> tokens = StorageService.instance.getPrimaryRanges(keyspaceName);
         if (!primaryRangeOnly)
@@ -49,41 +55,36 @@ public class DefaultAutoRepairTokenSplitter implements IAutoRepairTokenRangeSpli
             // if we need to repair non-primary token ranges, then change the tokens accrodingly
             tokens = StorageService.instance.getLocalReplicas(keyspaceName).ranges();
         }
-        AutoRepairConfig config = AutoRepairService.instance.getAutoRepairConfig();
         int numberOfSubranges = config.getRepairSubRangeNum(repairType);
+
+        boolean byKeyspace = config.getRepairByKeyspace(repairType);
+
+        // collect all token ranges.
+        List<Range<Token>> allRanges = new ArrayList<>();
         for (Range<Token> token : tokens)
         {
-            Murmur3Partitioner.LongToken l = (Murmur3Partitioner.LongToken) (token.left);
-            Murmur3Partitioner.LongToken r = (Murmur3Partitioner.LongToken) (token.right);
-            Token parentStartToken = StorageService.instance.getTokenMetadata()
-                                     .partitioner.getTokenFactory().fromString("" + l.getTokenValue());
-            Token parentEndToken = StorageService.instance.getTokenMetadata()
-                                   .partitioner.getTokenFactory().fromString("" + r.getTokenValue());
-            logger.debug("Parent Token Left side {}, right side {}", parentStartToken.toString(),
-                         parentEndToken.toString());
+            allRanges.addAll(splitEvenly(token, numberOfSubranges));
+        }
 
-            long left = (Long) l.getTokenValue();
-            long right = (Long) r.getTokenValue();
-            long repairTokenWidth = (right - left) / numberOfSubranges;
-            for (int i = 0; i < numberOfSubranges; i++)
+        if (byKeyspace)
+        {
+            for (Range<Token> splitRange : allRanges)
             {
-                long curLeft = left + (i * repairTokenWidth);
-                long curRight = curLeft + repairTokenWidth;
-
-                if ((i + 1) == numberOfSubranges)
-                {
-                    curRight = right;
-                }
-
-                Token childStartToken = StorageService.instance.getTokenMetadata()
-                                        .partitioner.getTokenFactory().fromString("" + curLeft);
-                Token childEndToken = StorageService.instance.getTokenMetadata()
-                                      .partitioner.getTokenFactory().fromString("" + curRight);
-                logger.debug("Current Token Left side {}, right side {}", childStartToken
-                                                                          .toString(), childEndToken.toString());
-                range.add(Pair.create(childStartToken, childEndToken));
+                // add repair assignment for each range entire keyspace's tables
+                repairAssignments.add(new RepairAssignment(splitRange, keyspaceName, tableNames));
             }
         }
-        return range;
+        else
+        {
+            // add repair assignment per table
+            for (String tableName : tableNames)
+            {
+                for (Range<Token> splitRange : allRanges)
+                {
+                    repairAssignments.add(new RepairAssignment(splitRange, keyspaceName, Collections.singleton(tableName)));
+                }
+            }
+        }
+        return repairAssignments;
     }
 }

src/java/org/apache/cassandra/repair/autorepair/IAutoRepairTokenRangeSplitter.java
@@ -19,13 +19,83 @@ package org.apache.cassandra.repair.autorepair;
 
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
+import org.apache.cassandra.dht.Range;
 import org.apache.cassandra.dht.Token;
 import org.apache.cassandra.utils.Pair;
 
 public interface IAutoRepairTokenRangeSplitter
 {
-    // split the token range you wish to repair into multiple subranges
-    // the autorepair framework will repair the list of returned subrange in a sequence
-    List<Pair<Token, Token>> getRange(AutoRepairConfig.RepairType repairType, boolean primaryRangeOnly, String keyspaceName, String tableName);
+
+    /**
+     * Split the token range you wish to repair into multiple assignments.
+     * The autorepair framework will repair the list of returned subrange in a sequence.
+     * @param repairType The type of repair being executed
+     * @param primaryRangeOnly Whether to repair only this node's primary ranges or all of its ranges.
+     * @param keyspaceName The keyspace being repaired
+     * @param tableNames The tables to repair
+     * @return repair assignments broken up by range, keyspace and tables.
+     */
+    List<RepairAssignment> getRepairAssignments(AutoRepairConfig.RepairType repairType, boolean primaryRangeOnly, String keyspaceName, Set<String> tableNames);
+
+    /**
+     * Defines a repair assignment to be issued by the autorepair framework.
+     */
+    class RepairAssignment
+    {
+        final Range<Token> tokenRange;
+
+        final String keyspaceName;
+
+        final Set<String> tableNames;
+
+        public RepairAssignment(Range<Token> tokenRange, String keyspaceName, Set<String> tableNames)
+        {
+            this.tokenRange = tokenRange;
+            this.keyspaceName = keyspaceName;
+            this.tableNames = tableNames;
+        }
+
+        public Range<Token> getTokenRange()
+        {
+            return tokenRange;
+        }
+
+        public String getKeyspaceName()
+        {
+            return keyspaceName;
+        }
+
+        public Set<String> getTableNames()
+        {
+            return tableNames;
+        }
+
+        @Override
+        public boolean equals(Object o)
+        {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            RepairAssignment that = (RepairAssignment) o;
+            return Objects.equals(tokenRange, that.tokenRange) && Objects.equals(keyspaceName, that.keyspaceName) && Objects.equals(tableNames, that.tableNames);
+        }
+
+        @Override
+        public int hashCode()
+        {
+            return Objects.hash(tokenRange, keyspaceName, tableNames);
+        }
+
+        @Override
+        public String toString()
+        {
+            return "RepairAssignment{" +
+                   "tokenRange=" + tokenRange +
+                   ", keyspaceName='" + keyspaceName + '\'' +
+                   ", tableNames=" + tableNames +
+                   '}';
+        }
+    }
 }

DataDog/cassandra-cep-37-auto-repair

src/java/org/apache/cassandra/repair/autorepair/DefaultAutoRepairTokenSplitter.java
@@ -20,28 +20,24 @@ package org.apache.cassandra.repair.autorepair;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.cassandra.service.AutoRepairService;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.apache.cassandra.dht.Murmur3Partitioner;
 import org.apache.cassandra.dht.Range;
 import org.apache.cassandra.dht.Token;
 import org.apache.cassandra.service.StorageService;
-import org.apache.cassandra.utils.Pair;
+
+import static org.apache.cassandra.repair.autorepair.AutoRepairUtils.splitEvenly;
 
 public class DefaultAutoRepairTokenSplitter implements IAutoRepairTokenRangeSplitter
 {
-    private static final Logger logger = LoggerFactory.getLogger(DefaultAutoRepairTokenSplitter.class);
-
-
     @Override
-    public List<Pair<Token, Token>> getRange(AutoRepairConfig.RepairType repairType, boolean primaryRangeOnly, String keyspaceName, String tableName)
+    public List<RepairAssignment> getRepairAssignments(AutoRepairConfig.RepairType repairType, boolean primaryRangeOnly, String keyspaceName, List<String> tableNames)
     {
-        List<Pair<Token, Token>> range = new ArrayList<>();
+        AutoRepairConfig config = AutoRepairService.instance.getAutoRepairConfig();
+        List<RepairAssignment> repairAssignments = new ArrayList<>();
 
         Collection<Range<Token>> tokens = StorageService.instance.getPrimaryRanges(keyspaceName);
         if (!primaryRangeOnly)
@@ -49,41 +45,36 @@ public class DefaultAutoRepairTokenSplitter implements IAutoRepairTokenRangeSpli
             // if we need to repair non-primary token ranges, then change the tokens accrodingly
             tokens = StorageService.instance.getLocalReplicas(keyspaceName).ranges();
         }
-        AutoRepairConfig config = AutoRepairService.instance.getAutoRepairConfig();
         int numberOfSubranges = config.getRepairSubRangeNum(repairType);
+
+        boolean byKeyspace = config.getRepairByKeyspace(repairType);
+
+        // collect all token ranges.
+        List<Range<Token>> allRanges = new ArrayList<>();
         for (Range<Token> token : tokens)
         {
-            Murmur3Partitioner.LongToken l = (Murmur3Partitioner.LongToken) (token.left);
-            Murmur3Partitioner.LongToken r = (Murmur3Partitioner.LongToken) (token.right);
-            Token parentStartToken = StorageService.instance.getTokenMetadata()
-                                     .partitioner.getTokenFactory().fromString("" + l.getTokenValue());
-            Token parentEndToken = StorageService.instance.getTokenMetadata()
-                                   .partitioner.getTokenFactory().fromString("" + r.getTokenValue());
-            logger.debug("Parent Token Left side {}, right side {}", parentStartToken.toString(),
-                         parentEndToken.toString());
+            allRanges.addAll(splitEvenly(token, numberOfSubranges));
+        }
 
-            long left = (Long) l.getTokenValue();
-            long right = (Long) r.getTokenValue();
-            long repairTokenWidth = (right - left) / numberOfSubranges;
-            for (int i = 0; i < numberOfSubranges; i++)
+        if (byKeyspace)
+        {
+            for (Range<Token> splitRange : allRanges)
             {
-                long curLeft = left + (i * repairTokenWidth);
-                long curRight = curLeft + repairTokenWidth;
-
-                if ((i + 1) == numberOfSubranges)
-                {
-                    curRight = right;
-                }
-
-                Token childStartToken = StorageService.instance.getTokenMetadata()
-                                        .partitioner.getTokenFactory().fromString("" + curLeft);
-                Token childEndToken = StorageService.instance.getTokenMetadata()
-                                      .partitioner.getTokenFactory().fromString("" + curRight);
-                logger.debug("Current Token Left side {}, right side {}", childStartToken
-                                                                          .toString(), childEndToken.toString());
-                range.add(Pair.create(childStartToken, childEndToken));
+                // add repair assignment for each range entire keyspace's tables
+                repairAssignments.add(new RepairAssignment(splitRange, keyspaceName, tableNames));
             }
         }
-        return range;
+        else
+        {
+            // add repair assignment per table
+            for (String tableName : tableNames)
+            {
+                for (Range<Token> splitRange : allRanges)
+                {
+                    repairAssignments.add(new RepairAssignment(splitRange, keyspaceName, Collections.singletonList(tableName)));
+                }
+            }
+        }
+        return repairAssignments;
     }
 }

src/java/org/apache/cassandra/repair/autorepair/IAutoRepairTokenRangeSplitter.java
@@ -19,13 +19,81 @@ package org.apache.cassandra.repair.autorepair;
 
 
 import java.util.List;
+import java.util.Objects;
 
+import org.apache.cassandra.dht.Range;
 import org.apache.cassandra.dht.Token;
-import org.apache.cassandra.utils.Pair;
 
 public interface IAutoRepairTokenRangeSplitter
 {
-    // split the token range you wish to repair into multiple subranges
-    // the autorepair framework will repair the list of returned subrange in a sequence
-    List<Pair<Token, Token>> getRange(AutoRepairConfig.RepairType repairType, boolean primaryRangeOnly, String keyspaceName, String tableName);
+
+    /**
+     * Split the token range you wish to repair into multiple assignments.
+     * The autorepair framework will repair the list of returned subrange in a sequence.
+     * @param repairType The type of repair being executed
+     * @param primaryRangeOnly Whether to repair only this node's primary ranges or all of its ranges.
+     * @param keyspaceName The keyspace being repaired
+     * @param tableNames The tables to repair
+     * @return repair assignments broken up by range, keyspace and tables.
+     */
+    List<RepairAssignment> getRepairAssignments(AutoRepairConfig.RepairType repairType, boolean primaryRangeOnly, String keyspaceName, List<String> tableNames);
+
+    /**
+     * Defines a repair assignment to be issued by the autorepair framework.
+     */
+    class RepairAssignment
+    {
+        final Range<Token> tokenRange;
+
+        final String keyspaceName;
+
+        final List<String> tableNames;
+
+        public RepairAssignment(Range<Token> tokenRange, String keyspaceName, List<String> tableNames)
+        {
+            this.tokenRange = tokenRange;
+            this.keyspaceName = keyspaceName;
+            this.tableNames = tableNames;
+        }
+
+        public Range<Token> getTokenRange()
+        {
+            return tokenRange;
+        }
+
+        public String getKeyspaceName()
+        {
+            return keyspaceName;
+        }
+
+        public List<String> getTableNames()
+        {
+            return tableNames;
+        }
+
+        @Override
+        public boolean equals(Object o)
+        {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            RepairAssignment that = (RepairAssignment) o;
+            return Objects.equals(tokenRange, that.tokenRange) && Objects.equals(keyspaceName, that.keyspaceName) && Objects.equals(tableNames, that.tableNames);
+        }
+
+        @Override
+        public int hashCode()
+        {
+            return Objects.hash(tokenRange, keyspaceName, tableNames);
+        }
+
+        @Override
+        public String toString()
+        {
+            return "RepairAssignment{" +
+                   "tokenRange=" + tokenRange +
+                   ", keyspaceName='" + keyspaceName + '\'' +
+                   ", tableNames=" + tableNames +
+                   '}';
+        }
+    }
 }